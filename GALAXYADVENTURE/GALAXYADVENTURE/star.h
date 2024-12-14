76e391064ec43c5affd53a1bcb8e1f8bbfaabf0e
wjdrmrgns <rmrgns@naver.com>
2024-12-15 04:32:02 +0900

    star 코드 개선
    생성자에서 쉐이더ID를 받도록 수정

--- GALAXYADVENTURE/GALAXYADVENTURE/star.h
+++ GALAXYADVENTURE/GALAXYADVENTURE/star.h
@@ -9,6 +9,7 @@ class Star : public Shape
 private:
 	Model model;
 	const char* filename = "OBJ/sphere1.obj";
+	GLuint shaderProgramID;
 
 	glm::vec3 pos = glm::vec3(0.f,1.1f,0.f);			// position
 	glm::vec3 size = glm::vec3(10.0f, 10.0f, 10.0f);		// size
@@ -20,22 +21,27 @@ private:
 	// light
 	GLfloat emissionPower = 0.5f; // power
 	float lightCycle  = 0;
+
+	// texture
+	int textureNum = 0;
+
 public:
-	Star() {
-		model.loadFromFile(filename);
-		
+	Star(GLuint shaderProgram)
+		: shaderProgramID(shaderProgram),         // shaderProgram 초기화
+		textureNum(RandomTexture()),          // RandomTexture 함수로 텍스처 번호 초기화
+		Transform(glm::translate(glm::mat4(1.0f), pos)), // 위치 변환 매트릭스 초기화
+		Scale(glm::scale(glm::mat4(1.0f), size)),        // 크기 변환 매트릭스 초기화
+		Matrix(Transform* Scale)            // 최종 변환 매트릭스 초기화
+	{
 		RandomColor(color);
 		RandomPosition(pos);
-		Transform = glm::translate(Transform, pos);
-		Scale = glm::scale(Scale, size);
-		Matrix = Transform * Scale;
-
+		model.loadFromFile(filename);           // 모델 로드
 	}
 	~Star() {
 
 	}
 
-	void Draw(GLuint shaderProgramID, GLuint transformLoc);
+	void Draw(GLuint transformLoc);
 
 	void Update(float time);
 
@@ -43,5 +49,7 @@ public:
 	Model getModel() const { return model; }
 	void setPos(glm::vec3 pos) { pos = pos; }
 	glm::vec3 getPos() const { return pos; }
+
+	int getTexNum() const { return textureNum; }
 };
 
