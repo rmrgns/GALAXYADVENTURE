e6b030307e11fb91f1f61442f5850e7de9800b5c
wjdrmrgns <rmrgns@naver.com>
2024-12-15 14:17:48 +0900

    meteor의 크기 변경

--- GALAXYADVENTURE/GALAXYADVENTURE/meteor.cpp
+++ GALAXYADVENTURE/GALAXYADVENTURE/meteor.cpp
@@ -4,7 +4,7 @@
 void Meteor::Draw(GLuint shaderProgramID, GLuint transformLoc) const
 {
 	glUniform1f(glGetUniformLocation(shaderProgramID, "emissionPower"), 0.5);
-	glUniform3fv(glGetUniformLocation(shaderProgramID, "objectColor"), 1, glm::value_ptr(color));
+	//glUniform3fv(glGetUniformLocation(shaderProgramID, "objectColor"), 1, glm::value_ptr(color));
 	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(Matrix));
 	CreateModel(vaoStar, vboStar, eboStar, model);
 	glBindVertexArray(vaoStar);
--- GALAXYADVENTURE/GALAXYADVENTURE/meteor.h
+++ GALAXYADVENTURE/GALAXYADVENTURE/meteor.h
@@ -8,11 +8,10 @@ class Meteor : public Shape
 {
 private:
 	Model model;
-	//const char* filename = "OBJ/RockSet.obj";
 	const char* filename = "OBJ/meteor.obj";
 
-	glm::vec3 pos = glm::vec3(0.f, 1.1f, 0.f);			// position
-	glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f);		// size
+	glm::vec3 pos = glm::vec3(0.f, 0.f, 0.f);			// position
+	glm::vec3 size = glm::vec3(5.0f, 5.0f, 5.0f);		// size
 	glm::vec3 color = glm::vec3(1.0f, 0.0f, 0.0f);		// color
 	glm::mat4 Matrix = glm::mat4(1.f);					// calculate matrix
 	glm::mat4 Transform = glm::mat4(1.f);				// transform matrix
